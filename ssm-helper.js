8bfabc41b9b8fa3fb3c72b3def21f50989bb411b
khuezy <khuezy.nguyen@gmail.com>
2022-01-17 09:58:22 -0800

    initial

---
+++ ssm-helper.js
@@ -0,0 +1,25 @@
+const AWS = require('aws-sdk')
+
+const getParameters = async (ssmPath, getChildren, decryption, region) => {
+    AWS.config.update({region: region})
+    const ssm = new AWS.SSM()
+    let params
+    let promise
+    if (getChildren) {
+        params = {
+            Path: ssmPath,
+            WithDecryption: decryption
+        };
+        promise = ssm.getParametersByPath(params).promise()
+    } else {
+        params = {
+            Names: [ssmPath],
+            WithDecryption: decryption
+        };
+        promise = ssm.getParameters(params).promise()
+    }
+    const result = await promise
+    return result.Parameters
+}
+
+module.exports = {getParameters}
